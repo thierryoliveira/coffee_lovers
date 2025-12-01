3c0c8649e20f337f8481317c669d4743235bdd12
Thierry Oliveira <thierry.ifsp@gmail.com>
2025-12-01 12:20:05 -0300

    fix: extend equatable on the Either implementation

--- lib/core/either/either.dart
+++ lib/core/either/either.dart
@@ -1,9 +1,14 @@
+import 'package:equatable/equatable.dart';
+
 /// An implementation of the Either type to represent values with two possibilities.
-abstract class Either<L, R> {
+abstract class Either<L, R> extends Equatable {
   const Either();
 
   /// Applies one of the provided functions based on whether the instance is Left or Right.
   T fold<T>(T Function(L left) onLeft, T Function(R right) onRight);
+
+  @override
+  List<Object?> get props => [];
 }
 
 /// Represents the left value of an Either type.
@@ -14,6 +19,9 @@ class Left<L, R> extends Either<L, R> {
   @override
   T fold<T>(T Function(L left) onLeft, T Function(R right) onRight) =>
       onLeft(value);
+
+  @override
+  List<Object?> get props => [value];
 }
 
 /// Represents the right value of an Either type.
@@ -24,6 +32,9 @@ class Right<L, R> extends Either<L, R> {
   @override
   T fold<T>(T Function(L left) onLeft, T Function(R right) onRight) =>
       onRight(value);
+
+  @override
+  List<Object?> get props => [value];
 }
 
 extension EitherUtils<L, R> on Either<L, R> {
